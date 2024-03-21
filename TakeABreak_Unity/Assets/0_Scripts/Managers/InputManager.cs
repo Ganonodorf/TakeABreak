03f7f6cf969bd568b9aa23ed27f612063efbb3e6
Ganonodorf <juanjogarridobascaran@gmail.com>
2024-03-21 10:43:57 +0100

    Refactorizacion

--- TakeABreak_Unity/Assets/0_Scripts/Managers/InputManager.cs
+++ TakeABreak_Unity/Assets/0_Scripts/Managers/InputManager.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationController : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    private Animator animator;
-
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
