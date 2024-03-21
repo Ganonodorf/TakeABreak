409c005d76d2505f114991f3ed43ea0cc710bab6
Ganonodorf <juanjogarridobascaran@gmail.com>
2024-03-21 11:25:14 +0100

    Metido todo el sistema de inputs

--- TakeABreak_Unity/Assets/0_Scripts/Managers/InputManager.cs
+++ TakeABreak_Unity/Assets/0_Scripts/Managers/InputManager.cs
@@ -1,18 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static InputManager Instance;
+
+    public ControlesJugador controlesJugador;
+
+    private void Awake()
     {
-        
+        HacerInmortal();
+
+        InicializarVariables();
+
+        SuscribirseEventos();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GameManager_CambioEstadoJuego(EstadoJuego nuevoEstadoJuego)
     {
-        
+        switch(nuevoEstadoJuego)
+        {
+            case EstadoJuego.Andando:
+                EnablearControlesAndando();
+                break;
+            case EstadoJuego.Conversando:
+                EnablearControlesConversando();
+                break;
+            case EstadoJuego.Eligiendo:
+                EnablearControlesEligiendo();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void EnablearControlesAndando()
+    {
+        controlesJugador.Andando.Enable();
+        controlesJugador.Conversando.Disable();
+    }
+
+    private void EnablearControlesConversando()
+    {
+        controlesJugador.Andando.Disable();
+        controlesJugador.Conversando.Enable();
+    }
+
+    private void EnablearControlesEligiendo()
+    {
+        controlesJugador.Andando.Disable();
+        controlesJugador.Conversando.Enable();
+    }
+
+    private void HacerInmortal()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void InicializarVariables()
+    {
+        controlesJugador = new ControlesJugador();
+    }
+
+    private void SuscribirseEventos()
+    {
+        GameManager.CambioEstadoJuego += GameManager_CambioEstadoJuego;
     }
 }
